0a2b002fbf90a8d06434e454b5a006850975ea5d
Kane.Pham <duyhoa149@gmail.com>
2021-12-06 00:37:56 +0700

    .

--- Selenium/webDriver/Topic_06_Web_Browser_Command.java
+++ Selenium/webDriver/Topic_06_Web_Browser_Command.java
@@ -1,6 +1,7 @@
 package webDriver;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
@@ -86,39 +87,160 @@ public class Topic_06_Web_Browser_Command {
 	@Test
 	public void TC_01_Element_IsDisplay() {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
-		assertTrue(driver.findElement(By.xpath("//label[@for='email']")).isDisplayed());
-		WebElement TestEmail = driver
-				.findElement(By.xpath("//legend[contains(.,'Your basic info')]/following-sibling::label[@for='mail']"));
-		if (TestEmail.isDisplayed()) {
-			driver.findElement(By.xpath("//input[@type='email'and@name='user_email']")).clear();
-			driver.findElement(By.xpath("//input[@type='email'and@name='user_email']")).sendKeys("Automation Testing");
-		}
 
-		assertTrue(driver.findElement(By.xpath("//label[@for='under_18']")).isDisplayed());
-		WebElement TestAge = driver.findElement(By.xpath("//label[contains(.,'Age')]"));
+		WebElement TestEmail = driver.findElement(By.xpath("//input[@name='user_email']"));
+		if (TestEmail.isDisplayed()) {
+			TestEmail.clear();
+			TestEmail.sendKeys("Automation Testing");
+			System.out.println("Email is displayed");
+		} else
+			System.out.println("Email is not displayed");
+
+		WebElement TestAge = driver.findElement(By.xpath("//input[@id='under_18']"));
 		if (TestAge.isDisplayed()) {
-			if (TestAge.findElement(By.xpath("//label[@for='under_18']")).isDisplayed()) {
-				TestAge.findElement(By.xpath("//label[@for='under_18']")).click();
-			}
-		}
-		WebElement TestEdu = driver
-				.findElement(By.xpath("//legend[contains(.,'Your profile')]/following-sibling::label[@for='edu']"));
+			TestAge.click();
+			System.out.println("Age is displayed");
+		} else
+			System.out.println("Age is not displayed");
+
+		WebElement TestEdu = driver.findElement(By.xpath("//textarea[@name='user_edu']"));
 		if (TestEdu.isDisplayed()) {
-			driver.findElement(By.xpath("//textarea[@id='edu']")).clear();
-			driver.findElement(By.xpath("//textarea[@id='edu']")).sendKeys("Automation Testing");
-		}
-		
-		WebElement TestDisplay = driver
-				.findElement(By.xpath("//h5[contains(.,'User5')]"));
-		if(TestDisplay.isDisplayed()) {
+			TestEdu.sendKeys("Automation Testing");
+			System.out.println("Education is displayed");
+		} else
+			System.out.println("Education is not displayed");
+
+		WebElement TestDisplay = driver.findElement(By.xpath("//h5[contains(.,'User5')]"));
+		if (TestDisplay.isDisplayed()) {
 			System.out.println("Element is displayed");
-		}
-		else {
+		} else
 			System.out.println("Element is not displayed");
-		}
 	}
 
-	@AfterClass
+	@Test
+	public void TC_02_Element_Check_Enable() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+
+		if (driver.findElement(By.xpath("//input[@name='user_email']")).isEnabled()) {
+			System.out.println("Email is Enabled");
+		} else
+			System.out.println("Email is not Enabled");
+
+		if (driver.findElement(By.xpath("//input[@id='under_18']")).isEnabled()) {
+			System.out.println("Age is Enabled");
+		} else
+			System.out.println("Age is not Enabled");
+
+		if (driver.findElement(By.xpath("//textarea[@name='user_edu']")).isEnabled()) {
+			System.out.println("Education is Enabled");
+		} else
+			System.out.println("Education is not Enabled");
+
+		if (driver.findElement(By.xpath("//select[@id='job1']")).isEnabled()) {
+			System.out.println("Job1 is Enabled");
+		} else
+			System.out.println("Job1 is not Enabled");
+
+		if (driver.findElement(By.xpath("//select[@id='job2']")).isEnabled()) {
+			System.out.println("Job2 is Enabled");
+		} else
+			System.out.println("Job2 is not Enabled");
+
+		if (driver.findElement(By.xpath("//input[@id='development']")).isEnabled()) {
+			System.out.println("Interests is Enabled");
+		} else
+			System.out.println("Interests is not Enabled");
+
+		if (driver.findElement(By.xpath("//input[@id='slider-1']")).isEnabled()) {
+			System.out.println("Slider is Enabled");
+		} else
+			System.out.println("Slider is not Enabled");
+
+		////
+		if (driver.findElement(By.xpath("//input[@name='user_pass']")).isEnabled()) {
+			System.out.println("PASS is Enabled");
+		} else
+			System.out.println("PASS is not Enabled");
+
+		if (driver.findElement(By.xpath("//input[@id='radio-disabled']")).isEnabled()) {
+			System.out.println("Age is Enabled");
+		} else
+			System.out.println("Age is not Enabled");
+
+		if (driver.findElement(By.xpath("//textarea[@name='user_bio']")).isEnabled()) {
+			System.out.println("Education is Enabled");
+		} else
+			System.out.println("Education is not Enabled");
+
+		if (driver.findElement(By.xpath("//select[@id='job3']")).isEnabled()) {
+			System.out.println("Job3 is Enabled");
+		} else
+			System.out.println("Job3 is not Enabled");
+
+		if (driver.findElement(By.xpath("//input[@id='check-disbaled']")).isEnabled()) {
+			System.out.println("Interests is Enabled");
+		} else
+			System.out.println("Interests is not Enabled");
+
+		if (driver.findElement(By.xpath("//input[@id='slider-2']")).isEnabled()) {
+			System.out.println("Slider is Enabled");
+		} else
+			System.out.println("Slider is not Enabled");
+	}
+
+	@Test
+	public void TC_03_Element_IsSelected() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+
+		WebElement TestAge = driver.findElement(By.xpath("//input[@id='under_18']"));
+		WebElement TestLanguage = driver.findElement(By.xpath("//input[@id='java']"));
+		//
+		TestAge.click();
+		TestLanguage.click();
+		//
+		if (TestAge.isSelected())
+			System.out.println("Age is Selected");
+		else
+			System.out.println("Age is not Selected");
+		
+		if (TestLanguage.isSelected())
+			System.out.println("Age is Selected");
+		else
+			System.out.println("Age is not Selected");
+		//
+		TestLanguage.click();
+		//
+		if (TestLanguage.isSelected())
+			System.out.println("Age is Selected");
+		else
+			System.out.println("Age is not Selected");
+	}
+	
+	@Test
+	public void TC_04_Element_Register_Function() {
+		driver.get("https://login.mailchimp.com/signup/");
+		
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("Autotest@gmail.com");
+		driver.findElement(By.xpath("//input[@id='new_username']")).sendKeys("Auto Test");
+		WebElement pass = driver.findElement(By.xpath("//input[@id='new_password']"));
+		pass.sendKeys("1");
+		assertFalse(driver.findElement(By.xpath("//li[@class='number-char']")).isDisplayed());
+		pass.sendKeys("a");
+		assertFalse(driver.findElement(By.xpath("//li[@class='lowercase-char']")).isDisplayed());
+		pass.sendKeys("A");
+		assertFalse(driver.findElement(By.xpath("//li[@class='uppercase-char']")).isDisplayed());
+		pass.sendKeys("@");
+		assertFalse(driver.findElement(By.xpath("//li[@class='special-char']")).isDisplayed());
+		pass.sendKeys("1111");
+		assertFalse(driver.findElement(By.xpath("//button[@id='create-account']']")).isDisplayed());
+		//pass.sendKeys("111111111111111111111111111111111111111111111111111111");
+		//assertFalse(driver.findElement(By.xpath("//li[@class='plus-50 error']")).isDisplayed());
+		
+		sleepInSecond(2);
+		
+	}
+
+	@AfterClass 	
 	public void afterClass() {
 		driver.quit();
 	}
