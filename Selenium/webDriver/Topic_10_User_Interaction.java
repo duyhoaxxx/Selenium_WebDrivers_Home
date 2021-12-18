6505604c26249fad5be185017bf6d135d0ad6c12
Kane.Pham <duyhoa149@gmail.com>
2021-12-18 20:23:00 +0700

    Finish Topic10

--- Selenium/webDriver/Topic_10_User_Interaction.java
+++ Selenium/webDriver/Topic_10_User_Interaction.java
@@ -1,12 +1,25 @@
 package webDriver;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -26,6 +39,7 @@ public class Topic_10_User_Interaction {
 	WebDriverWait explicitWait;
 	JavascriptExecutor jsExcutor;
 	String projectPath = System.getProperty("user.dir");
+	String dragDropHelperPath = projectPath+"\\DragDropHTML5\\drag_and_drop_helper.js";
 
 	@BeforeClass
 	public void beforeClass() {
@@ -137,13 +151,107 @@ public class Topic_10_User_Interaction {
 	}
 
 	@Test
-	public void TC_09_Drag_And_Drop_Element_HTML5() {
+	public void TC_09_Drag_And_Drop_Element_HTML5() throws IOException  {
 		driver.get("https://automationfc.github.io/drag-drop-html5/");
+		sleepInSecond(2);
+		String collumACss = "#column-a";
+		String collumBCss = "#column-b";
+		String dragDropHelperContent = getContentFile(dragDropHelperPath);
+		
+		dragDropHelperContent = dragDropHelperContent + "$(\"" + collumACss + "\").simulateDragDrop({ dropTarget: \"" + collumBCss + "\"});";
+		// A=>B
+		jsExcutor.executeScript(dragDropHelperContent);
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-a>header")).getText(), "B");
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-b>header")).getText(), "A");
+		// B=>A
+		jsExcutor.executeScript(dragDropHelperContent);
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-a>header")).getText(), "A");
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-b>header")).getText(), "B");
 	}
 
+	@Test
+	public void TC_09_Drag_And_Drop_Element_HTML5_Xpath() throws AWTException {
+		driver.get("https://automationfc.github.io/drag-drop-html5/");
+		sleepInSecond(2);
+		String collumAXpath = "//div[@id='column-a']";
+		String collumBXpath = "//div[@id='column-b']";
+		
+		dragAndDropHTML5ByXpath(collumAXpath,collumBXpath);
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-a>header")).getText(), "B");
+		Assert.assertEquals(driver.findElement(By.cssSelector("#column-b>header")).getText(), "A");
+	}
+	
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
+	}
+	
+	public void dragAndDropHTML5ByXpath(String sourceLocator, String targetLocator) throws AWTException {
+
+		WebElement source = driver.findElement(By.xpath(sourceLocator));
+		WebElement target = driver.findElement(By.xpath(targetLocator));
+
+		// Setup robot
+		Robot robot = new Robot();
+		robot.setAutoDelay(500);
+
+		// Get size of elements
+		Dimension sourceSize = source.getSize();
+		Dimension targetSize = target.getSize();
+
+		// Get center distance
+		int xCentreSource = sourceSize.width / 2;
+		int yCentreSource = sourceSize.height / 2;
+		int xCentreTarget = targetSize.width / 2;
+		int yCentreTarget = targetSize.height / 2;
+
+		Point sourceLocation = source.getLocation();
+		Point targetLocation = target.getLocation();
+		System.out.println(sourceLocation.toString());
+		System.out.println(targetLocation.toString());
+
+		// Make Mouse coordinate center of element
+		sourceLocation.x += 20 + xCentreSource;
+		sourceLocation.y += 110 + yCentreSource;
+		targetLocation.x += 20 + xCentreTarget;
+		targetLocation.y += 110 + yCentreTarget;
+
+		System.out.println(sourceLocation.toString());
+		System.out.println(targetLocation.toString());
+
+		// Move mouse to drag from location
+		robot.mouseMove(sourceLocation.x, sourceLocation.y);
+
+		// Click and drag
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.mouseMove(((sourceLocation.x - targetLocation.x) / 2) + targetLocation.x, ((sourceLocation.y - targetLocation.y) / 2) + targetLocation.y);
+
+		// Move to final position
+		robot.mouseMove(targetLocation.x, targetLocation.y);
+
+		// Drop
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+	
+	public String getContentFile(String filePath) throws IOException {
+		Charset cs = Charset.forName("UTF-8");
+		FileInputStream stream = new FileInputStream(filePath);
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+			StringBuilder builder = new StringBuilder();
+			char[] buffer = new char[8192];
+			int read;
+			while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+				builder.append(buffer, 0, read);
+			}
+			return builder.toString();
+		} finally {
+			stream.close();
+		}
 	}
 
 	public void SetBrowser() {
